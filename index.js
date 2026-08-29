4162b0acbfd40ec782016b8405763e4548534e33
nymulislam <naymulislam241@gmail.com>
2026-08-29 13:10:20 +0600

    chore: update server setup to connect to MongoDB and modify package dependencies

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Middleware
+app.use(express.json());
+
+const client = new MongoClient(process.env.MONGODB_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+async function run() {
+    try {
+        
+        await client.connect();
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+       
+        app.get('/', (req, res) => {
+            res.send('E-Page server is running and database is connected!');
+        });
+
+     
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`);
+        });
+
+    } catch (error) {
+        console.error("Failed to connect to MongoDB", error);
+    }
+}
+
+run().catch(console.dir);
