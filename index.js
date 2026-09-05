c2f77e0cd4e1cc1671431f05d15777f33bb60cfb
nymulislam <naymulislam241@gmail.com>
2026-09-05 23:47:19 +0600

    chore: comment out MongoDB connection in run function

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const client = new MongoClient(process.env.MONGODB_URI, {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
         console.log("Connected to MongoDB!");
 
         const database = client.db("e-page_db");
